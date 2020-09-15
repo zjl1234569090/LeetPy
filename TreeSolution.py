cee602ea33314ff91c9d8839c4ce50e1b4a474e1
omegazengjl <omegazengjl@gmail.com>
2020-09-15 15:58:40 +0800

    update

--- TreeSolution.py
+++ TreeSolution.py
@@ -94,30 +94,38 @@ class Solution:
         return 1 + max(self.max_depth(root.left), self.max_depth(root.right))
 
     def is_balanced(self, root: TreeNode) -> bool:
-        def depth(root:TreeNode):
-            if root is None:
-                return 0, True
+        if root is None:
+            return True
+        left_depth = self.max_depth(root.left)
+        right_depth = self.max_depth(root.right)
+        return self.is_balanced(root.left) and self.is_balanced(
+            root.right) and abs(left_depth - right_depth) <= 1
 
-            dl, bl = depth(root.left)
-            dr, br = depth(root.right)
-
-            return max(dl, dr) + 1, bl and br and abs(dl - dr) < 2
-
-        _, out = depth(root)
-        return out
-
-    @staticmethod
-    def max_path_sum(root: TreeNode):
+    def max_path_sum(self, root: TreeNode):
+        """
+        给定一个非空二叉树，返回其最大路径和.
+        思路：分治法，分为三种情况：左子树最大路径和最大，右子树最大路径和最大，
+        左右子树最大加根节点最大，需要保存两个变量：一个保存子树最大路径和，
+        一个保存左右加根节点和，然后比较这个两个变量选择最大值即可
+        :param root:
+        :return:
+        """
         max_path = float('-inf')
         if root is None:
             return float('-inf')
-        e_l = Solution.max_path_sum(root.left)
-        e_r = Solution.max_path_sum(root.right)
+        e_l = self.max_path_sum(root.left)
+        e_r = self.max_path_sum(root.right)
         max_path = max(max_path, root.val + max(0, e_l) + max(0, e_r), e_l, e_r)
         return max_path
 
-    def lowest_common_ancestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-
+    def lowest_common_ancestor(self, root: TreeNode, p: TreeNode, q: TreeNode):
+        """
+        给定一个二叉树, 找到该树中两个指定节点的最近公共祖先.
+        :param root:
+        :param p:
+        :param q:
+        :return:
+        """
         if root is None:
             return None
 
@@ -136,7 +144,13 @@ class Solution:
         else:
             return None
 
-    def insert_into_bst(self, root: TreeNode, val: int) -> TreeNode:
+    def insert_into_bst(self, root: TreeNode, val: int):
+        """
+        给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点.
+        :param root:
+        :param val:
+        :return:
+        """
 
         if root is None:
             return TreeNode(val)
